37b9da56a4f90c4629eb4540218a24bd91a74951
JordyV23 <jordyvargas0123@gmail.com>
2024-04-10 22:04:30 -0600

    Mejoras de interfaz
     - Creacion de contenido personalizado
    - Interfaz mas amigable y responsiva
    - Inclusion de colores personalizados
    - Implementacion de mas secciones
    - Inlcusion de iconografia de la marca

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			"SquidInk":"#202A36",
+			"Apricot":"#f9c6b2",
+			"Opal":"#9ec3c1",
+			"Salmon":"#f5a782",
+			"White":"#FFFFFF",
+		}
 	},
 	plugins: [],
 }
